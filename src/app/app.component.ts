02e0a7ec7fdae1c5e00a5a6d72e33a00d8df56a3
Bruno <ferreira.brunomarcelo@gmail.com>
2021-09-21 21:20:13 -0300

    refactor: update title page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'knewin-noticias-infomoney-frontend';
+
+  public constructor(private title: Title) {
+
+  }
+
+  ngOnInit(): void { 
+    this.title.setTitle("Infomoney Notícias");
+  }
+ 
 }
